6e3ceb98e01320072737e6d926136f0d9a29654e
Zeng Ke <superisaac.ke@gmail.com>
2022-02-16 19:43:20 +0800

    find all and find only one results

--- ocaml/README.md
+++ ocaml/README.md
@@ -9,5 +9,8 @@
 
 # usage
 ```shell
-% dune exec ./twentyfour.exe 6 2 3 7
+% dune exec ./twentyfour.exe 6 2 3 7        #
+or
+% dune exec ./twentyfour.exe all 6 2 3 7    # print all results
+
 ```
--- ocaml/twentyfour.ml
+++ ocaml/twentyfour.ml
@@ -22,12 +22,45 @@ let search (inputs:int list) : F.formula option =
   |> List.flatten                              (* flatten to formula list *)
   |> List.find_map F.check                     (* formula option *)
 
-let () =
-  let inputs = Sys.argv
-               |> fun x -> Array.sub x 1 4
-               |> Array.to_list
-               |> List.map int_of_string in
-  match search inputs with
-  | Some f -> print_endline(F.to_str f)
-  | None -> print_endline "None"
+(* run the whole search task *)
+let search_all (inputs:int list) : F.formula option list =
+  (* intermediate variables *)
+  let opslist : B.binop list list =
+    B.gen_binops 3
+  in
 
+  inputs
+  |> permutations                              (* int list list *)
+  |> List.map (F.build_formula_list opslist)   (* formula list list *)
+  |> List.flatten                              (* flatten to formula list *)
+  |> List.map F.check                          (* formula option *)
+;;
+
+let print_formula_opt fopt =
+  match fopt with
+  | Some f -> print_endline(F.to_str f)
+  | _ -> ()
+
+
+let find_all (inputs: int list): unit =
+  search_all inputs
+  |> List.iter print_formula_opt
+;;
+
+let find_one (inputs: int list): unit =
+  search inputs |> print_formula_opt
+;;
+
+let () =
+  let cmd = Array.get Sys.argv 1 in
+  match cmd with
+  | "all" ->
+     (Array.sub Sys.argv 2 4)
+     |> Array.to_list
+     |> List.map int_of_string
+     |> find_all;
+  | _ ->
+     Array.sub Sys.argv 1 4
+     |> Array.to_list
+     |> List.map int_of_string
+     |> find_one
